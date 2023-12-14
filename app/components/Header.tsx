a8281f3bd285e610e861934344f174dfc306f77f
Faotu Happy <79374710+Faotu@users.noreply.github.com>
2023-12-15 00:45:58 +0100

    framer motion animation

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import { FaCode, FaGithubSquare } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
 import { FaSquareXTwitter } from "react-icons/fa6";
+import { motion } from "framer-motion";
 
 interface HeaderProps {
   children: React.ReactNode;
@@ -26,13 +27,26 @@ const activeRoute = [
 
 const Header: React.FC<HeaderProps> = ({ children, className }) => {
   return (
-    <header className="sticky top-0 p-5">
-      <div className="flex flex-col px-4 py-6">
-        <div className="pl-6 flex flex-row ">
-          <FaCode size={26} className="text-teal-600 " />
-          <h1 className="pl-4 text-lg">FaotuHappy</h1>
-          <div className="">
-            {" "}
+    <header className="sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center">
+      <motion.div
+        initial={{
+          x: -500,
+          opacity: 0,
+          scale: 0.5,
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1,
+        }}
+        transition={{
+          duration: 2.5,
+        }}
+      >
+        <div className="flex flex-col px-4 py-6">
+          <div className="pl-6 flex flex-row ">
+            <FaCode size={26} className="text-teal-600 " />
+            <h1 className="pl-4 text-lg">FaotuHappy</h1>
             <div className="flex flex-row justify-items-end  px-8 gap-4 pl-60 ">
               {activeRoute.map((item) => (
                 <div
@@ -66,7 +80,7 @@ const Header: React.FC<HeaderProps> = ({ children, className }) => {
             </div>
           </div>
         </div>
-      </div>
+      </motion.div>
     </header>
   );
 };
