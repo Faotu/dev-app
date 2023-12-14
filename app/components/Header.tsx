1c07e703655d5f428a66d362cc687cb9e751c5d0
Faotu Happy <79374710+Faotu@users.noreply.github.com>
2023-12-15 00:03:09 +0100

    updated

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -26,32 +26,44 @@ const activeRoute = [
 
 const Header: React.FC<HeaderProps> = ({ children, className }) => {
   return (
-    <header>
+    <header className="sticky top-0 p-5">
       <div className="flex flex-col px-4 py-6">
-        <div className="pl-6 flex flex-row">
+        <div className="pl-6 flex flex-row ">
           <FaCode size={26} className="text-teal-600 " />
           <h1 className="pl-4 text-lg">FaotuHappy</h1>
-          <div className="flex flex-row items-center px-8 gap-4 pl-6 ">
-            {activeRoute.map((item) => (
-              <div
-                className="hover:text-teal-500 hover:text-xl cursor-pointer transition"
-                key={item.id}
-              >
-                {item.label}
-              </div>
-            ))}
-          </div>
-          <div className="flex items-end px-4 gap-5">
-            <Link href="https://www.twitter.com/FaotuHappy/">
-              <FaSquareXTwitter size={26} />
-            </Link>
+          <div className="">
+            {" "}
+            <div className="flex flex-row justify-items-end  px-8 gap-4 pl-60 ">
+              {activeRoute.map((item) => (
+                <div
+                  className="hover:text-teal-500 hover:text-xl cursor-pointer transition"
+                  key={item.id}
+                >
+                  {item.label}
+                </div>
+              ))}
+            </div>
+            <div className="flex items-start justify-right text-right px-4 gap-5">
+              <Link href="https://www.twitter.com/FaotuHappy/">
+                <FaSquareXTwitter
+                  size={26}
+                  className="hover:text-xl hover:text-teal-500"
+                />
+              </Link>
 
-            <Link href="https://www.linkedin.com/in/faotuhappy/">
-              <FaLinkedin size={26} />
-            </Link>
-            <Link href="https://github.com/Faotu">
-              <FaGithubSquare size={26} />
-            </Link>
+              <Link href="https://www.linkedin.com/in/faotuhappy/">
+                <FaLinkedin
+                  size={26}
+                  className="hover:text-xl hover:text-teal-500"
+                />
+              </Link>
+              <Link href="https://github.com/Faotu">
+                <FaGithubSquare
+                  size={26}
+                  className="hover:text-xl hover:text-teal-500"
+                />
+              </Link>
+            </div>
           </div>
         </div>
       </div>
