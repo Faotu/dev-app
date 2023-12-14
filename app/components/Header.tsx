bc29d81ebbb3a7fa01b7126e3c6f94cf2544321e
Faotu Happy <79374710+Faotu@users.noreply.github.com>
2023-12-14 23:16:48 +0100

    links

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,6 +1,9 @@
 "use client";
+import Link from "next/link";
 import React from "react";
-import { FaCode } from "react-icons/fa";
+import { FaCode, FaGithubSquare } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { FaSquareXTwitter } from "react-icons/fa6";
 
 interface HeaderProps {
   children: React.ReactNode;
@@ -23,17 +26,36 @@ const activeRoute = [
 
 const Header: React.FC<HeaderProps> = ({ children, className }) => {
   return (
-    <div className="flex flex-col px-4 py-6">
-      <div className="pl-6 flex flex-row">
-        <FaCode size={26} className="text-teal-600 " />
-        <h1 className="pl-4 text-lg">FaotuHappy</h1>
-        <div className="flex flex-row items-center px-8 gap-4 pl-6">
-          {activeRoute.map((item) => (
-            <div key={item.id}>{item.label}</div>
-          ))}
+    <header>
+      <div className="flex flex-col px-4 py-6">
+        <div className="pl-6 flex flex-row">
+          <FaCode size={26} className="text-teal-600 " />
+          <h1 className="pl-4 text-lg">FaotuHappy</h1>
+          <div className="flex flex-row items-center px-8 gap-4 pl-6 ">
+            {activeRoute.map((item) => (
+              <div
+                className="hover:text-teal-500 hover:text-xl cursor-pointer transition"
+                key={item.id}
+              >
+                {item.label}
+              </div>
+            ))}
+          </div>
+          <div className="flex items-end px-4 gap-5">
+            <Link href="https://www.twitter.com/FaotuHappy/">
+              <FaSquareXTwitter size={26} />
+            </Link>
+
+            <Link href="https://www.linkedin.com/in/faotuhappy/">
+              <FaLinkedin size={26} />
+            </Link>
+            <Link href="https://github.com/Faotu">
+              <FaGithubSquare size={26} />
+            </Link>
+          </div>
         </div>
       </div>
-    </div>
+    </header>
   );
 };
 
